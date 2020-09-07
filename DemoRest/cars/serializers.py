45e67b074eabfb481361a47c42ec9f6a0de0a161
DimaLat <dima.latyshionok@gmail.com>
2020-09-07 13:59:12 +0300

    add view and serializer

---
+++ DemoRest/cars/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from cars.models import Car
+
+
+class CarDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car # указываем модель к которой сериализатор будет привязан
+        fields = '__all__' # указываем, что работаем (сериаллизуем) со всеми полями

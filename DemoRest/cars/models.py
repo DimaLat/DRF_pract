67bbaaecc4ce491c758668f7db44a6ff7324d9df
DimaLat <dima.latyshionok@gmail.com>
2020-09-08 10:29:40 +0300

    redact car type field

--- DemoRest/cars/models.py
+++ DemoRest/cars/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 # Create your models here.
 
 class Car(models.Model):
-    vin = models.CharField(verbose_name='Vin', db_index=True, unique= True, max_length=64)
+    vin = models.CharField(verbose_name='Vin', db_index=True, unique=True, max_length=64)
     color = models.CharField(verbose_name='Color', max_length=64)
     brand = models.CharField(verbose_name='Brand', max_length=64)
     CAR_TYPES = (
@@ -15,6 +15,8 @@ class Car(models.Model):
         (2, 'Хэтчбэк'),
         (3, 'Универсал'),
         (4, 'Купе'),
+        (5, 'Минивэн'),
+        (6, 'Внедорожник'),
     )
     car_type = models.IntegerField(verbose_name='Car_Type', choices=CAR_TYPES)
     user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
